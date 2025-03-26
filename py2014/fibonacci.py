b254127e7f128f5e09e46b179e219d465e47d391
Anastasiia <140421314+errreway@users.noreply.github.com>
2025-03-26 13:56:11 +0300

    Update fibonacci.py

--- py2014/fibonacci.py
+++ py2014/fibonacci.py
@@ -5,10 +5,7 @@ def fib1(n):
     ''' normal recursion '''
     if n == 0:
         return 0
-    elif n == 1:
-        return 1
-    else:
-        return fib1(n - 1) + fib1(n - 2)
+   
 
 
 known = {0: 0, 1: 1}
